69492013cbf94d94fb0a28412e702206975c083a
mateuszstaszkow <mateuszstasz@gmail.com>
2024-07-10 13:35:22 +0200

    feat: setup deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    reactStrictMode: true,
+    assetPrefix: isProd ? '/ms-portfolio/' : '',
+    trailingSlash: true,
+};
 
 export default nextConfig;
