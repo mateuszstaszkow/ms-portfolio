a3622a2e3626b1b951a97febfc302b6205fa007c
mateuszstaszkow <mateuszstasz@gmail.com>
2024-07-20 12:43:37 +0200

    fix: lint

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import SubPageHeader from "@/components/shared/SubPageHeader/SubPageHeader";
-import { Project } from "@/components/Projects/model/projects.interface";
-import { PROJECTS } from "@/data/projects";
-import ProjectGrid from "@/components/Projects/ProjectGrid/ProjectGrid";
+import React from 'react';
+import SubPageHeader from '@/components/shared/SubPageHeader/SubPageHeader';
+import { Project } from '@/components/Projects/model/projects.interface';
+import { PROJECTS } from '@/data/projects';
+import ProjectGrid from '@/components/Projects/ProjectGrid/ProjectGrid';
 
 const AVERAGE_DAYS_IN_MONTH: number = 30.437;
 const MILLISECONDS_IN_MONTH: number = 1000 * 3600 * 24 * AVERAGE_DAYS_IN_MONTH;
@@ -13,20 +13,20 @@ const Projects: React.FC = () => {
     const month: string = parts[0];
     const year: string = parts[1];
     return new Date(`${year}-${month}${day ? `-${day}` : ''}`);
-  }
+  };
 
   const getLastTimestamp = (project: Project): number => {
     const dates: string[] = project.dates.split(' ');
-    return mapToDate(dates.at(-1)).getTime();
-  }
+    return mapToDate(dates.at(-1) as string).getTime();
+  };
 
-  const getMonthsDifference = (project): number => {
+  const getMonthsDifference = (project: Project): string => {
     const dates: string[] = project.dates.split(' ');
     if (dates.length === 1) {
       return '';
     }
     const firstDate = mapToDate(dates[0]);
-    const lastDate = mapToDate(dates.at(-1), 28);
+    const lastDate = mapToDate(dates.at(-1) as string, 28);
     const differenceMs = lastDate.getTime() - firstDate.getTime();
     const differenceMonths = Math.ceil(differenceMs / MILLISECONDS_IN_MONTH);
     const differenceMonthString = `${differenceMonths % 12} month${differenceMonths >= 2 ? 's' : ''}`;
@@ -35,16 +35,23 @@ const Projects: React.FC = () => {
     } else {
       return differenceMonthString;
     }
-  }
+  };
 
-  const projects: Project[] = [...PROJECTS].reverse()
+  const projects: Project[] = [...PROJECTS]
+    .reverse()
     .sort((a, b) => getLastTimestamp(b) - getLastTimestamp(a))
-    .map(project => ({ ...project, monthsDifference: getMonthsDifference(project) }));
+    .map((project) => ({
+      ...project,
+      monthsDifference: getMonthsDifference(project)
+    }));
 
   return (
     <div>
-      <SubPageHeader id="projects" title="Projects" label={`${projects.length} commercial & private projects`}>
-      </SubPageHeader>
+      <SubPageHeader
+        id="projects"
+        title="Projects"
+        label={`${projects.length} commercial & private projects`}
+      ></SubPageHeader>
       <ProjectGrid projects={projects}></ProjectGrid>
     </div>
   );
--- src/components/Projects/model/projects.interface.ts
+++ src/components/Projects/model/projects.interface.ts
@@ -7,5 +7,5 @@ export interface Project {
   important?: boolean;
   countryCode?: string;
   isPublication?: boolean;
-  monthsDifference?: number;
+  monthsDifference?: string;
 }
